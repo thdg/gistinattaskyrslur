10d8d7a9288f961a298565801feefaa4941d7e8d
Þorsteinn Daði Gunnarsson <thorsteinng@ru.is>
2021-06-15 18:15:23 +0000

    Save files in a safe way

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 from datetime import datetime
 from flask import Flask, render_template, request
 from main import read_reservations, calculate_stats, print_stats
@@ -6,12 +7,17 @@ from main import read_reservations, calculate_stats, print_stats
 app = Flask(__name__)
 UPLOAD_FOLDER = "media/"
 
+
+def save_fname(original):
+    return str(uuid.uuid4()) +"-" + original
+
+
 @app.route("/", methods=['POST', 'GET'])
 def index():
     stats = None
     if request.method == "POST":
         fin = request.files["file"]
-        fname = os.path.join(UPLOAD_FOLDER, fin.filename)
+        fname = os.path.join(UPLOAD_FOLDER, save_fname(fin.filename))
         fin.save(fname)
         with open(fname) as csvfile:
             lines = read_reservations(csvfile)
